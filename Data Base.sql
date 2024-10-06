4431a2bb28b417dec512f5e9467657831afb217f
RogerMacia <roger.macia@estudiantat.upc.edu>
2024-10-06 20:45:18 +0200

    Update Data Base

--- Data Base.sql
+++ Data Base.sql
@@ -1,9 +1,6 @@
-DROP DATABASE IF EXISTS Tanks;
-CREATE DATABASE Tanks;
-USE Tanks;
-
 CREATE TABLE Player (id INT PRIMARY KEY, name TEXT, score INT);
-CREATE TABLE Game (id INT PRIMARY KEY, id_Winner INT);
+CREATE TABLE Map (id INT PRIMARY KEY);
+CREATE TABLE Game (id INT PRIMARY KEY, id_Winner INT, id_Map INT, FOREIGN KEY (id_Map) REFERENCES Map (id));
 CREATE TABLE Participant (id_P INT, id_G INT, FOREIGN KEY (id_P) REFERENCES Player (id), FOREIGN KEY (id_G) REFERENCES Game (id));
 
 INSERT INTO Player (id, name, score) VALUES (1, 'A', 10);
@@ -12,9 +9,13 @@ INSERT INTO Player (id, name, score) VALUES (3, 'C', 2);
 INSERT INTO Player (id, name, score) VALUES (4, 'D', 0);
 INSERT INTO Player (id, name, score) VALUES (5, 'E', 12);
 
-INSERT INTO Game (id, id_Winner) VALUES (1, 2);
-INSERT INTO Game (id, id_Winner) VALUES (2, 5);
-INSERT INTO Game (id, id_Winner) VALUES (3, 5);
+INSERT INTO Map (id) VALUES (1);
+INSERT INTO Map (id) VALUES (2);
+INSERT INTO Map (id) VALUES (3);
+
+INSERT INTO Game (id, id_Winner, id_Map) VALUES (1, 2, 2);
+INSERT INTO Game (id, id_Winner, id_Map) VALUES (2, 5, 1);
+INSERT INTO Game (id, id_Winner, id_Map) VALUES (3, 5, 3);
 
 INSERT INTO Participant (id_P, id_G) VALUES (1, 1);
 INSERT INTO Participant (id_P, id_G) VALUES (2, 1);
